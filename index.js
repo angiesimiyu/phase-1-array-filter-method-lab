d710960a14407c8e050815608773be91667eb2cb
angelinesimiyu <angie.lukelas@gmail.com>
2022-11-29 10:00:06 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(array, name){
+    return array.filter(function (driver){
+        return driver.toLowerCase() === name.toLowerCase()
+    })
+};
+findMatching(drivers, 'Bobby')
+
+function fuzzyMatch(array, name){
+    return array.filter(function (driver){
+        return driver.toLowerCase().substring(0, name.length) === name.toLowerCase()
+    })
+};
+fuzzyMatch(drivers, 'Bobby')
+
+function matchName(drivers, argument) {
+    return drivers.filter(function (driver) {
+        return driver.name === argument
+    })
+};
